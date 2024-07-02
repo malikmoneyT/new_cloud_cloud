5b8ddff8f57cf623ae79c70b4f4ca5fc37f5a0a7
malikmoneyT <95098139+malikmoneyT@users.noreply.github.com>
2024-07-02 00:47:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import boto3
 from templates import *
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 
 app = Flask(__name__)
